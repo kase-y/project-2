f11664d043990040a34c054e6417e42095aeffe5
kase-y <69023154+kase-y@users.noreply.github.com>
2020-10-27 20:43:40 -0400

    made some changes

--- content.js
+++ content.js
@@ -1,6 +1,17 @@
-var elements = document.getElementsByTagName('p');
+var elements = document.getElementsByTagName('div');
 
 for (var i = 0; i < elements.length; i++) {
    var element = elements[i];
+//   console.log( element );
+//element.style.setProperty('border', 'solid 1px red');
+   
+   var r = Math.floor(Math.random()*255);
+   var g = Math.floor(Math.random()*255);
+   var b = Math.floor(Math.random()*255);
+   var c = "rgb(" + r + "," + g + "," + b +")";
+
+   element.style.setProperty("border","solid 1px " + c);
+
+
 }
 
