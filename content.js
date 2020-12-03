425c3dea60f9f2f62097a90f1271b26346dbae00
kase-y <69023154+kase-y@users.noreply.github.com>
2020-12-02 23:32:34 -0500

    adding some user options

--- content.js
+++ content.js
@@ -1,17 +1,38 @@
-var elements = document.getElementsByTagName('div');
+// var elements = document.getElementsByTagName('div');
 
-for (var i = 0; i < elements.length; i++) {
-   var element = elements[i];
-//   console.log( element );
-//element.style.setProperty('border', 'solid 1px red');
+// for (var i = 0; i < elements.length; i++) {
+//    var element = elements[i];
+// //   console.log( element );
+// //element.style.setProperty('border', 'solid 1px red');
    
-   var r = Math.floor(Math.random()*255);
-   var g = Math.floor(Math.random()*255);
-   var b = Math.floor(Math.random()*255);
-   var c = "rgb(" + r + "," + g + "," + b +")";
+//    var r = Math.floor(Math.random()*255);
+//    var g = Math.floor(Math.random()*255);
+//    var b = Math.floor(Math.random()*255);
+//    var c = "rgb(" + r + "," + g + "," + b +")";
 
-   element.style.setProperty("border","solid 1px " + c);
+//    element.style.setProperty("border","solid 1px " + c);
 
 
+// }
+
+// var myDiv = document.createElement("div");
+// myDiv.style.setProperty("postition", "absolute");
+// myDiv.style.setProperty("top", "100px");
+// myDiv.style.setProperty("left", "250px");
+// myDiv.style.setProperty("background", "rgb(255,0,0)")
+// myDiv.style.setProperty("width", "250px");
+// myDiv.style.setProperty("height", "250px");
+// myDiv.innerHTML ="HELLO";
+// document.body.appendChild(myDiv);
+
+chrome.storage.sync.get({fromStored:"", toStored:""}, function(result) {
+
+var html = document.querySelector('html');
+var walker = document.createTreeWalker(html,NodeFilter.SHOW_TEXT);
+var node;
+while (node = walker.nextNode()) {
+   var re = new RegExp(result.fromStored, "gi");
+   node.nodeValue = node.nodeValue.replace(re, result.toStored);
 }
 
+});
